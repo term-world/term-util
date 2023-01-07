c8371736e92720e8b290fc124f5d533c452206d6
dluman <dluman@allegheny.edu>
2023-01-07 11:49:52 -0500

    Updating to use fake UserAgent

--- libs/gitit/src/gitit.py
+++ libs/gitit/src/gitit.py
@@ -2,13 +2,18 @@ import requests
 
 from .Config import Config
 
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+
 def get(
       raw_path: str = Config.values["URL"],
       file_type: str = "objects",
       file_name: str = ""
     ) -> None:
     raw = requests.get(
-      f"{raw_path}/{file_type}/{file_name}"
+      f"{raw_path}/{file_type}/{file_name}",
+     headers = {'User-Agent': ua.chrome}
     )
     text = str(raw.text)
     file = open(file_name, "x")
