e3ff9d991878dc4c8cd9c7d42cc5e41e53f263c3
dluman <dluman@allegheny.edu>
2023-03-11 13:57:29 -0500

    MOving validator methods; not integrated or tested yet

--- libs/inventory/src/Validation.py
+++ libs/inventory/src/Validation.py
@@ -1,4 +1,23 @@
+import re
+import importlib
+
+from .Item import BoxSpec
+
 class Validator:
 
-    def __init__(self):
-        pass
+    @staticmethod
+    def validate(filename: str = "") -> bool:
+        try:
+            name, ext = filename.split("/")
+            if not ext == "py":
+                raise
+            obj = importlib.import_module(name)
+            getattr(obj, name).use()
+        except Exception as e:
+            print("Not a valid item file.")
+            return False
+        return True
+
+    @staticmethod
+    def is_box(item: any) -> bool:
+        return "BoxSpec" in dir(item)
