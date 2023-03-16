4a29a6d9a4ff5cc4f594c36d26eccf36bf2a0e48
dluman <dluman@allegheny.edu>
2023-03-15 22:16:01 -0400

    Cleaning up code; adding TODOs

--- libs/marketplace/src/listing.py
+++ libs/marketplace/src/listing.py
@@ -9,28 +9,22 @@ from couchsurf import Connection
 
 class Listing:
 
-    """ Require:
-        * author
-        * date
-        * version
-        * description
-    """
-
     def __init__(self, files: any = ""):
         if self.is_valid(files):
             print("[MARKETPLACE] Passed validation...")
             self.author = os.getlogin()
             self.date = datetime.now().timestamp()
-            self.name = str(input("[MARKETPLACE] Name of package to list: "))
+            # TODO: Suggest defaults based on file names and precalculated
+            #       values for name and author, for example.
+            self.name = input("[MARKETPLACE] Name of package to list: ")
 
     def serialize(self) -> dict:
         obj = importlib.import_module(self.name)
+        # Serializes the catalog record
         return {
-            "Module": self.name,
-            "Author": self.author,
-            "Date": self.date,
-            "Version": "v0.1.0",
-            "Description": getattr(obj, self.name)().use.__doc__
+            "package": self.name,
+            "author": self.author
+            "description": getattr(obj, self.name)().use.__doc__
         }
 
     def is_valid(self, files: any = "") -> bool:
@@ -40,18 +34,15 @@ class Listing:
             for parent, dirs, files in os.walk(files):
                 files = files
         for file in files:
-            # TODO: Convert Acquire.validate to bool?
-            #       This fixes the issue that only _one_ of the
-            #       files has to actually work -- and it tells us
-            #       which file to add the code to.
+            # Only 1 file has to validate for the package
+            # to be potentially valid
             if Validator.validate(file):
                 return True
-        # TODO: Remains valid for now (for testing)
         return False
 
     def is_version(self) -> str:
         conn = Connection("marketplace")
-        conn.request.get("listings")
+        conn.request.get("items")
 
     def pack(self):
         pack = Package(name = self.name, files = f"{self.name}.py")
