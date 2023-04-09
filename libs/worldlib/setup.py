08d8eb0d108331c30d5c1b471b5beb7c253747d0
dluman <dluman@allegheny.edu>
2023-04-09 14:37:16 -0400

    Updating most universal setup files

--- libs/worldlib/setup.py
+++ libs/worldlib/setup.py
@@ -4,7 +4,6 @@ setuptools.setup(
     name="worldlib",
     version="0.1",
     packages=['worldlib'],
-    package_dir={'worldlib': 'src'},
     include_package_data=True,
     description='Shorthand import for all world-specific libraries for term-world narrative objects.',
     long_description=open('README.md', 'r').read(),
