858b5599a7c5347851f937ae7370751da04f01fb
jmsjones12 <jones02@allegheny.edu>
2023-04-19 10:03:58 -0400

    Changes

--- libs/resources/setup.py
+++ libs/resources/setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="resources",
     version="0.1",
-    packages=['resources']
+    packages=['resources'],
     include_package_data=True,
     description='Global resource pool for term-world resources',
     long_description=open('README.md', 'r').read(),
