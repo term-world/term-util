c31566462c8d9720b8cc0a0d53050cddf565d1f9
Shnacko <tylerznaczko@gmail.com>
2023-04-21 15:19:58 -0400

    Splitting power

--- libs/resources/resources/powergrid.py
+++ libs/resources/resources/powergrid.py
@@ -3,14 +3,13 @@ import json
 class PowerGrid:
 
     def add_power(wind = 0, solar = 0, coal = 0, oil = 0, nuclear = 0, natural_gass = 0):
+        #more generalizable way to do this path
+        renewable_energy = wind + solar
+        exhaustable_energy = coal + oil + nuclear + natural_gass
         with open("../../term-util-power-planters/libs/resources/resources/worldbattery.json", "r") as fh:
             world_battery = json.load(fh)
-            world_battery["power"] += int(wind)
-            world_battery["power"] += solar
-            world_battery["power"] += coal
-            world_battery["power"] += oil
-            world_battery["power"] += nuclear
-            world_battery["power"] += natural_gass
+            world_battery["renewable_energy"] += renewable_energy
+            world_battery["exhaustable_energy"] += exhaustable_energy
         with open("../../term-util-power-planters/libs/resources/resources/worldbattery.json", "w") as add_battery:
             json.dump(world_battery, add_battery)
 
@@ -18,7 +17,10 @@ class PowerGrid:
     def use_power(power):
         with open("../term-util-power-planters/libs/resources/resources/worldbattery.json", "r") as fh:
             world_battery = json.load(fh)
+        if world_battery["power"] >= power:
             world_battery["power"] -= power
+        else:
+            print("There It Not Enough Power In The World To Run This! ")
         with open("../term-util-power-planters/libs/resources/resources/worldbattery.json", "w") as add_battery:
             json.dump(world_battery, add_battery)
 
