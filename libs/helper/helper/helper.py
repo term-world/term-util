b04457045610780a844b3770e8cde8e52b527015
cschwartz01 <schwartz01@allegheny.edu>
2023-04-26 10:24:27 -0400

    code review start

--- libs/helper/helper/helper.py
+++ libs/helper/helper/helper.py
@@ -1,4 +1,5 @@
 import os
+import os.path
 import openai
 import json
 
@@ -10,7 +11,7 @@ from rich.style import Style
 from .motd import *
 from .spinner import SpinThread
 
-from time import sleep 
+from time import sleep
 
 API = {
     "key": os.getenv("OPEN_AI_KEY"),
@@ -18,11 +19,9 @@ API = {
 }
 
 SYSTEM = """You are a civil servant named cliv3 who teaches the Python programming language.
-
 Town residents will ask for help with specific Python commands, and your job is to respond with kind,
 helpful messages with examples that relate to various town services such as bodega, datamart, woodshop, voting, 
 hall of records, datamart, water supply, the power grid, trash collection, or proper lawn care.
-
 If residents are rude to you, politely tell them they need to be kind and that you've reported them
 to the town mayor and refuse to answer the question, suggesting that they be a bit more neighborly.
 """
@@ -88,143 +87,84 @@ class Helper:
     def motd(self) -> None:
         self.render(msg)
 
+    def read_file(self) -> None:
+        print()
+        folder_name = input("What is the folder name? ")
+        file_name = input("What is the file name? ")
+        file_path = os.path.join(folder_name, file_name)
+        file_exist = os.path.exists(file_path)
+
+        # root_dir = folder_name
+        # for dir_path, dir_names, file_names in os.walk(root_dir):
+        #     for name in file_names:
+        #         new_path = os.path.join(root, name)
+
+        while file_exist == False:
+            ''' if file not in folder then add subfolder to file path '''
+            try:
+                inputs = [folder_name, file_name]
+                sub_folder = input("What is the subfolder name? ")
+                add_input = inputs.append(sub_folder)
+                new_path = str(input + ',')
+                file_paths = os.path.join(new_path)
+                file_exists = os.path.exists(file_paths)
+                dir_exists = os.path.isdir(sub_folder)
+                is_file = os.path.isfile(file_paths)
+                with open(file_paths, 'r') as file:
+                    content = file.read()
+                    self.query(content)
+                    self.console(clear)
+                    markdown = Markdown('\t' + content)
+                    self.console.print(markdown)
+            except:
+                """ currently only allows for one subfolder input, otherwise user recieves below error """
+                print(f"It looks like '{file_name}' does not exist in '{folder_name}' or '{sub_folder}'")
+                break 
+
+            #     folder_input = input("What is the subfolder name? ")
+            #     new_path = str(folder_input) + ' ,'
+            #     file_pathss = os.path.join(folder_name, sub_folder, subb_folder, file_name)
+            #     file_exists = os.path.exists(file_pathss)
+            #     dir_exists = os.path.isdir(subb_folder)
+            #     is_file = os.path.isfile(file_pathss)
+            #     with open(file_pathss, 'r') as file:
+            #         content = file.read()
+            #         self.query(content)
+            #         self.console(clear)
+            #         markdown = Markdown('\t' + content)
+            #         self.console.print(markdown)
+        
+        else: 
+            with open(file_path, 'r') as file:
+                content = file.read() 
+                self.query(content) 
+                self.console.clear()
+                markdown = Markdown('\t' + content)
+                self.console.print(markdown)
+        # folder_files = os.listdir(folder_name)
+        # print(folder_files)
+        # if folder
+        #     file_name = input("What is the file name? ")
+        #     file_path = os.path.join(folder_name, file_name)
+        #     with open(file_path, 'r') as file:
+        #         content = file.read()
+        #         self.query(content)
+        #         self.console(clear)
+        #         markdown = Markdown('\t' + content)
+        #         self.console.print(markdown)
+
     def chat(self) -> None:
         self.motd()
         while True:
             print()
             print()
-            # question = input("🤖 CLIV3: What Python topic would you like to ask about? ")
-            response = int(input("🤖 CLIV3: What Python topic can I help you with? "))
-            # if question.lower() == "q":
-            #     print("🤖 CLIV3: Goodbyte!")
-            #     break
-            if response == 1:
-                question = input("🤖 CLIV3: What Python topic would you like to ask about? ")
-                self.query(question)    
-            if response == 0:
+            question = input("🤖 CLIV3: What Python topic would you like to ask about? ")
+            if question == "code review":
+                self.read_file()
+            if question.lower() == "q":
                 print("🤖 CLIV3: Goodbyte!")
-                break 
-
-    class Example:
-
-        API = {
-        "key": os.getenv("OPEN_AI_KEY"),
-        "org": os.getenv("OPEN_AI_ORG")
-        }
-
-        SYSTEM = """You are a civil servant named cliv3 who teaches the Python programming language.
-
-        Town residents will ask for help with specific Python commands, and your job is to respond with kind,
-        helpful messages with examples that relate to various town services such as bodega, datamart, woodshop, voting, 
-        hall of records, datamart, water supply, the power grid, trash collection, or proper lawn care.
-
-        If residents are rude to you, politely tell them they need to be kind and that you've reported them
-        to the town mayor and refuse to answer the question, suggesting that they be a bit more neighborly.
-        """
-
-        PROMPTS = [
-            {"role": "system", "content": SYSTEM}
-        ]
-
-        openai.api_key = API["key"]
-        openai.api_org = API["org"]
-
-        def __init__(self):
-            super().__init__()
-
-        # def render(self, response: str = "") -> None:
-        #     markdown = Markdown('\r' + response)
-        #     self.console.print(markdown, soft_wrap = False, end = '\r')
-
-        def query(self,question: str = "") -> str:
-            PROMPTS.append(
-                {"role": "user", "content": question}
-                )
-            # adds question (from user input) to PROMPTS
-            responses = openai.ChatCompletion.create(
-                model= "gpt-4",
-                messages= PROMPTS,
-                temperature= 0.1,
-                stream = True,
-                n= 1
-                )
-            words = ""
-            response = self.parse_stream(responses)
-            for word in response:
-                # get the content out of response and print that 
-                # PROMPTS.append(word)
-                if self.parse_stream():
-                    print(word, end="", flush=True)
-                    words = words + word
-            self.console.clear()
-            markdown = Markdown('\t' + words)
-            print()
-            # print(words)        
-            self.console.print(markdown, soft_wrap=False, end='')
-
-        # def parse_stream(self, responses: dict = {}) -> str:
-        #     """ this is a generator """
-        #     for chunk in responses:
-        #         try:
-        #             msg = chunk["choices"][0]["delta"]["content"]
-        #             yield msg  
-        #         except KeyError:
-        #             pass
-
-        # def chat(self) -> None:
-        #     self.motd()
-        #     print("1. Question")
-        #     print("2. Code")
-        #     print("0. Exit")
-        #     while True:
-        #         print()
-        #         print()
-        #         # question = input("🤖 CLIV3: What Python topic would you like to ask about? ")
-        #         response = int(input("Choose an option by number: "))
-        #         # if question.lower() == "q":
-        #         #     print("🤖 CLIV3: Goodbyte!")
-        #         #     break
-        #         if response == 1:
-        #             question = input("🤖 CLIV3: What Python topic would you like to ask about? ")
-        #             self.query(question)
-        #         if response == 0:
-        #             print("🤖 CLIV3: Goodbyte!")
-        #             break    
-
-    def main():
-        print()
-        cliv3 = Example()
-        # cliv3.chat()
-
-
-    class Review:
-
-        API = {
-            "key": os.getenv("OPEN_AI_KEY"),
-            "org": os.getenv("OPEN_AI_ORG")
-        }
-
-        SYSTEM = """You are a civil servant named cliv3 who teaches the Python programming language.
-
-            Town residents will ask for help with specific Python code, and your job is to read the specific 
-            files that are requested of you and response with kind, helpful messages to help residents fix their code.
-
-            If residents are rude to you, politely tell them they need to be kind and that you've reported them
-            to the town mayor and refuse to answer the question, suggesting that they be a bit more neighborly.
-        """
-
-        PROMPTS = [
-            {"role": "system", "content": SYSTEM}
-        ]
-
-        openai.api_key = API["key"]
-        openai.api_org = API["org"]
-
-            # def read_file(self):
-
-            # def open_folder(self):
-
-    # def main(): 
+                break
+            self.query(question)
 
 def main():
     print()
