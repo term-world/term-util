42501101bb7c4a367d17258bd205ff3bc1f7cc9d
dluman <dluman@allegheny.edu>
2023-05-15 21:32:12 -0400

    Dynamically sensing next scene?

--- libs/narrator/narrator/Narrator.py
+++ libs/narrator/narrator/Narrator.py
@@ -8,21 +8,35 @@ class Narrator:
   def __init__(self):
     fh = open(".paths.yml")
     self.paths = yaml.safe_load(fh)
-    self.path = Path(list(self.paths.keys())[0],list(self.paths.values())[0])
+    self.path = Path(
+        paths = {
+          "act": list(self.paths.keys())[0],
+          "scene": list(self.paths.values())
+        }
+    )
 
   def narrate(self, all: bool = False):
     lines = []
     acts = list(self.paths)
-
-    chosen_path = self.paths[self.path.act] if self.path.act in acts else self.paths[acts[0]]
-
+    
+    # Try to choose a given path, if key exists
+    try:
+        chosen_path = self.paths[self.path.act] 
+    except KeyError:
+        chosen_path = self.paths[acts[0]]
+    
     if all:
       for scenes in list(chosen_path.values()):
         lines += scenes
     else:
       scenes = list(chosen_path)
-      lines = chosen_path[self.path.scene] if self.path.scene in scenes else chosen_path[scenes[0]]
+      try:
+        lines = chosen_path[self.path.scene]
+      except KeyError:
+        lines = chosen_path[scenes[0]]
 
     for line in lines:
       print(line)
       sleep(1)
+
+    self.path.get_next_scene(self.paths)
--- libs/narrator/narrator/Path.py
+++ libs/narrator/narrator/Path.py
@@ -1,14 +1,19 @@
-from collections import namedtuple
-
 class Path:
 
-  def __init__(self, act: str, scene: str):
-    self.act = act
-    self.scene = scene
+  def __init__(self, paths: dict = {}):
+    """ Constructor """
+    self.act = paths["act"]
+    self.scene = list(list(paths["scene"])[0].keys())[0]
 
-  def next_scene(self):
-    self.scene += 1
+  def get_next_scene(self, paths):
+    scenes = list(
+        paths[self.act]
+    )
+    idx = scenes.index(self.scene)
+    if(idx + 1 <= len(scenes)):
+        self.scene = paths[self.act][scenes[idx+1]]
 
-  def change(self, path):
+  def change(self, path: dict = {"act": 1, "scene": 1}):
+    """ Change a scene in a currently-running path """
     self.act = path["act"]
     self.scene = path["scene"]
