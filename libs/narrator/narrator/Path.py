8e5fb9302f10f840b5f8601c4fe003f160991229
dluman <dluman@allegheny.edu>
2023-05-15 21:37:46 -0400

    Adding more error handling

--- libs/narrator/narrator/Path.py
+++ libs/narrator/narrator/Path.py
@@ -6,12 +6,17 @@ class Path:
     self.scene = list(list(paths["scene"])[0].keys())[0]
 
   def get_next_scene(self, paths):
+    """ If there's another scene to go to, well, go to it! """
     scenes = list(
         paths[self.act]
     )
-    idx = scenes.index(self.scene)
-    if(idx + 1 <= len(scenes)):
-        self.scene = paths[self.act][scenes[idx+1]]
+    try:
+        idx = scenes.index(self.scene)
+    except ValueError:
+        return
+    idx += 1
+    if(idx <= len(scenes)):
+        self.scene = paths[self.act][scenes[idx]]
 
   def change(self, path: dict = {"act": 1, "scene": 1}):
     """ Change a scene in a currently-running path """
