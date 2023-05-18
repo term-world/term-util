75465a3784be1fc28179c731dcc14a0ac41bd2ad
dluman <dluman@allegheny.edu>
2023-05-18 15:12:43 -0400

    Adding NPC generation

--- libs/gremlin/gremlin/__init__.py
+++ libs/gremlin/gremlin/__init__.py
@@ -1,1 +1,9 @@
-from .npc import NPC
+from .npc import *
+
+API = {
+    "key": os.getenv("OPEN_AI_KEY"),
+    "org": os.getenv("OPEN_AI_ORG")
+}
+
+openai.api_key = API["key"]
+openai.api_org = API["org"]
--- libs/gremlin/gremlin/npc.py
+++ libs/gremlin/gremlin/npc.py
@@ -1,41 +1,21 @@
 import os
 import openai
 
-from couchsurf import Connection
+from .catalog import Entry
 
 class NPC:
-    
-    API = {
-        "key": os.getenv("OPEN_AI_KEY"),
-        "org": os.getenv("OPEN_AI_ORG")
-    }
 
-    HEALTH = 1
-    ATTACK = 0
-    DEFEND = 0
+    def __init__(self, name: str = "", **kwargs):
+        self.__entry = Entry(name = name, **kwargs)
+        self.__stats(*self.__entry.data)
 
-    def __init__(self, name: str = ""):
-        self.conn = Connection("npcs")
-        self.__query(name = name)
-    
-    def __query(self, **kwargs):
-        terms = {
-            "type": {"op": "EQUALS", "arg": "npc"}
-        }
-        for arg in kwargs:
-            terms.update({
-                arg:{"op": "LIKE", "arg": kwargs[arg]}
+    def __stats(self, data: dict = {}, **kwargs):
+        for prop in kwargs:
+            data.update({
+                prop: kwargs[prop]
             })
-        self.__run(terms)
+        for prop in data:
+            setattr(self, prop, data[prop])
     
-    def __run(self, terms: dict = {}):
-        result = self.conn.request.query(
-            **terms
-        )
-        return result["docs"]
-
-    def __register(self):
-        pass
-    
-    def __unregister(self):
-        pass
+    def update(self):
+        self.__entry.update(self)
