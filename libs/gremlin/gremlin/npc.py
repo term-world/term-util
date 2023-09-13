44bb692dbf9d0b63c4d274c11fe2b1962365f8cf
Charlie <schwartz01@allegheny.edu>
2023-09-13 16:11:05 -0400

    more comments

--- libs/gremlin/gremlin/npc.py
+++ libs/gremlin/gremlin/npc.py
@@ -11,18 +11,19 @@ class NPC: # use NPC as a super class for other characters?
         # **kwargs is used when you're unsure about the number of arguments to pass in function
         # using **kwargs creates a dictionary of arguments
         self.__entry = Entry(name = name, **kwargs) 
-        # creates attribute called __entry
+        # creates attribute called __entry, assigns value of class Entry
         self.__stats(*self.__entry.data)
-        # creates attribute called __stats 
+        # creates attribute called __stats
 
     def __stats(self, data: dict = {}, **kwargs):
-    ''' stats method '''
-        for prop in kwargs:
-            data.update({
+    ''' stats method, take dictionary called 'data' as parameter  '''
+        for prop in kwargs: # for property in dictionary (?) '**kwargs'
+            data.update({ 
                 prop: kwargs[prop]
-            })
-        for prop in data:
-            setattr(self, prop, data[prop])
+            }) 
+        for prop in data: # for property in dictionary 'data'
+            setattr(self, prop, data[prop]) 
+            # sets property attribute object NPC to value property in dictionary 'data'
     
     def update(self):
-        self.__entry.update(self)
+        self.__entry.update(self) # update entry for NPC object
