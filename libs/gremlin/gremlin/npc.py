c929af50fed5ed12044947f280dfb45ff7408606
Charlie <schwartz01@allegheny.edu>
2023-09-13 16:01:13 -0400

    comments

--- libs/gremlin/gremlin/npc.py
+++ libs/gremlin/gremlin/npc.py
@@ -1,15 +1,22 @@
 import os
 import openai
 
-from .catalog import Entry
+from .catalog import Entry # from catalog import class Entry
+from helper import Helper # if i wanted to import Helper class
 
-class NPC:
+class NPC: # use NPC as a super class for other characters?
 
-    def __init__(self, name: str = "", **kwargs):
-        self.__entry = Entry(name = name, **kwargs)
+    def __init__(self, name: str = "", **kwargs): 
+    ''' intializes the attribites (variables) of object NPC '''
+        # **kwargs is used when you're unsure about the number of arguments to pass in function
+        # using **kwargs creates a dictionary of arguments
+        self.__entry = Entry(name = name, **kwargs) 
+        # creates attribute called __entry
         self.__stats(*self.__entry.data)
+        # creates attribute called __stats 
 
     def __stats(self, data: dict = {}, **kwargs):
+    ''' stats method '''
         for prop in kwargs:
             data.update({
                 prop: kwargs[prop]
